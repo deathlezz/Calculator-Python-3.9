2f778c6c7a8547d40a3e2d4a8e47905062423a87
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-01-30 16:06:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+# Simple calculator
+# Python 3.9.1
+# Created by deathlezz
+# Date 24.01.2021
+
+
+# Create add function
+def add(x, y):
+    return x + y
+
+# Create subtract function
+def subtract(x, y):
+    return x - y
+
+# Create multiply function
+def multiply(x, y):
+    return x * y
+
+# Create divide function
+def divide(x, y):
+    return x / y
+
+# Say hi
+print(" ")
+print("* Welcome to calculator *")
+print(" ")
+print("Select operation:")
+print("[1] Add / [2] Subtract / [3] Multiply / [4] Divide")
+print(" ")
+
+while True:
+
+    # Create operation choice
+    choice = input("Enter your choice: ")
+    choice2 = ()
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+
+        try:
+            num1 = round(float(input("Enter first number: ")), 2)
+
+        except ValueError:
+            print('# Enter numbers only')
+            print('')
+            continue
+
+        try:
+            num2 = round(float(input("Enter second number: ")), 2)
+
+        except ValueError:
+            print('# Enter numbers only')
+            print('')
+            continue
+
+        if choice == '1':
+            print("Solution:", num1, "+", num2, "=", round(add(num1, num2), 2))
+            print('')
+
+        elif choice == '2':
+            print("Solution:", num1, "-", num2, "=", round(subtract(num1, num2), 2))
+            print('')
+
+        elif choice == '3':
+            print("Solution:", num1, "x", num2, "=", round(multiply(num1, num2), 2))
+            print('')
+
+        elif choice == '4':
+            print("Solution:", num1, ":", num2, "=", round(divide(num1, num2), 2))
+            print('')
+
+        # There can be 'break' if you want to stop a loop
+
+    else:
+        print("# Enter number from 1 to 4")
+        print('')
